c12ce8f193c48808cb95882402f1f898575c9743
raskal-dev <kalvinrasamyzo@gmail.com>
2023-09-06 15:30:58 +0300

    migration du formation (table) et former (table)

---
+++ database/migrations/2023_09_06_121714_create_formateurs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formateurs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_form', 50);
+            $table->string('prenom_form', 100)->nullable();
+            $table->string('cin_form', 12);
+            $table->string('sexe', 1);
+            $table->date('date_nais_form');
+            $table->string('num_tel_form', 13);
+            $table->string('mail_form', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formateurs');
+    }
+};
---
+++ database/migrations/2023_09_06_121727_create_formers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_for')->constrained('formations')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('id_form')->constrained('formateurs')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formers');
+    }
+};
